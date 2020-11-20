0432140bfb81e0184acfdb2da5b52e85555d5293
Mark Price <mrprice1@comcast.net>
2020-11-20 17:09:02 -0500

    Initial commit.

---
+++ app/components/Nav.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+var { Link, IndexLink } = require('react-router');
+
+var Nav = () => {
+	return (
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">React Timer App</li>
+					<li>
+						<IndexLink
+							to="/"
+							activeClass="active"
+							activeStyle={{ fontWeight: 'bold' }}>Timer
+						</IndexLink>
+					</li>
+					<li>
+						<IndexLink
+							to="/"
+							activeClass="active"
+							activeStyle={{ fontWeight: 'bold' }}>Countdown
+						</IndexLink>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+				<div>
+					<ul className="menu">
+						<li className="menu-text">
+							Created by <a className="created-text">Mark Price</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+module.exports = Nav;
